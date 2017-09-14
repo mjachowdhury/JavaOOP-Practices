c999c498276033214ac312d76b03ca2e829b12c2
mjachowdhury <study.computing2016@gmail.com>
2017-09-14 21:11:13 +0100

    Added few comments

--- JavaProgrammingOopPractrices/src/shapeInterfaceExample/Rectangle.java
+++ JavaProgrammingOopPractrices/src/shapeInterfaceExample/Rectangle.java
@@ -3,6 +3,7 @@ package shapeInterfaceExample;
 //One child have only one parent but multiple child can have one parent as well 
 // But they can not have more then one parent
 //when us e interface can't use extent have to use implements
+//for the interface we can implements more then one class
 public class Rectangle implements Shape {
 
 	public Rectangle(){}
